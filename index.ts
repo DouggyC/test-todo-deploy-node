bd2648132715dd4815b87c3c182edbe37ef3f6e6
Student <student@mac.lan>
2026-02-23 17:05:51 +1100

    test deploy CD ECS

--- index.ts
+++ index.ts
@@ -4,21 +4,21 @@ import DB from './config/db';
 
 const app = express();
 
-// app.get('/todos', (req, res) => {
-//   res.json({
-//     todos: [{ id: 1, title: 'Buy groceries', completed: false }],
-//   });
-// });
-
-app.get('/todos', async (req, res) => {
-  try {
-    const result = await DB.query('SELECT * FROM todos');
-    res.json({ todos: result.rows });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database error' });
-  }
+app.get('/todos', (req, res) => {
+  res.json({
+    todos: [{ id: 1, title: 'Buy groceries', completed: false }],
+  });
 });
+
+// app.get('/todos', async (req, res) => {
+//   try {
+//     const result = await DB.query('SELECT * FROM todos');
+//     res.json({ todos: result.rows });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: 'Database error' });
+//   }
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
